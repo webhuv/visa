13bcdc82af7cb989e53c9423f4991f6781495cf2
suraj-b-chettri <suraj.bohara.589@gmail.com>
2018-09-10 03:24:32 -0700

    intrn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ApplyComponent } from './apply/apply.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ApplyComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      }, {
+        path: 'apply',
+        component: ApplyComponent,
+      }, {
+        path: 'form',
+        component: FormComponent,
+      }
+    ])
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
